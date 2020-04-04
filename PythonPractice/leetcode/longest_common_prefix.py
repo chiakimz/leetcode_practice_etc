4d69e31ff92f6aaaadc769af1a640b43d47c8bd3
Chiaki Mizuta <chiaki.mizuta@holition.com>
2020-04-04 17:27:00 +0100

    different angle, runs faster

--- PythonPractice/leetcode/longest_common_prefix.py
+++ PythonPractice/leetcode/longest_common_prefix.py
@@ -6,41 +6,23 @@
 class Solution:
     def longestCommonPrefix(self, strs):
         commonLetters = ""
-        if not strs or strs[0] == "":
-            return ""
-
-        # Find common prefixes in the first two strings
-
-        for s in range(len(strs[0])):
-            if len(strs) == 1:
-                return strs[0]
-            else:    
-                if s == len(strs[1]):
+        
+        s0 = strs[0]
+        length = len(s0)  
+        for l in range(length):
+            if not strs or strs[l] == "":
+                return ""
+            for s in strs[1:]:
+                what = s[:length]
+                whe = s[:length] == s0
+                if length > len(s) or s[:length] != s0:
                     break
-                if strs[0][s] == strs[1][s]:  
-                    commonLetters += strs[0][s]
-                else:
-                    break
-        if not commonLetters:
-            return ""     
+            if  strs.index(s) == len(strs) - 1:
+                break    
+            s0 = s0[: length - 1]
+            length -= 1
+        return s0    
 
-        #since first two are already checked, start from the index 2
-        if len(strs) > 2:
-            for i in range(2, len(strs)):
-                if strs[i] == "":    
-                    return ""
-                else:    
-                    for s in range(len(commonLetters)):
-                        if s == len(strs[i]):
-                            break
-                        if commonLetters[s] != strs[i][s]:
-                            commonLetters = commonLetters[:s]    
-                            break
-                        elif commonLetters[s] == strs[i][s] and s == len(strs[i]) - 1:
-                            commonLetters = commonLetters[:s + 1]  
-                            break
-
-        return commonLetters
 
 if __name__ == "__main__":
     sol = Solution()    
